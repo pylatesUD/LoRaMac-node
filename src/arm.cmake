ab631534ec404658b2435f9b04cb9c0b31dee2d8
ingelectronicadj <ingelectronicadj@gmail.com>
2017-11-10 18:27:50 -0500

    modificaciones generales

--- src/arm.cmake
+++ src/arm.cmake
@@ -1,5 +1,5 @@
 INCLUDE(CMakeForceCompiler)
- 
+
 SET(CMAKE_SYSTEM_NAME Generic)
 SET(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR cortex-m0plus)
@@ -7,7 +7,7 @@ set(CMAKE_SYSTEM_PROCESSOR cortex-m0plus)
 # specify the cross compiler
 set(CMAKE_C_COMPILER arm-none-eabi-gcc)
 set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
- 
+
 #SET(COMMON_FLAGS "-mcpu=cortex-m3 -mthumb -mthumb-interwork -msoft-float -ffunction-sections -fdata-sections -g -fno-common -fmessage-length=0 --specs=nosys.specs")
 
 SET(COMMON_FLAGS "-O4  -ansi -pedantic -Wall -W -Wshadow -Wcast-qual -Wwrite-strings -mthumb -mcpu=cortex-m0plus -fno-builtin -Wl,--gc-sections -ffunction-sections -fdata-sections -fomit-frame-pointer -mabi=aapcs -fno-unroll-loops -ffast-math -ftree-vectorize --specs=nano.specs --specs=nosys.specs")
